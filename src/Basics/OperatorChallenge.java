16c2017c085de218bd1f9988c9b518e20594f358
ibudnitski <igor.budnitski@gmail.com>
2023-12-14 17:19:16 +0200

    Commit

--- src/Basics/OperatorChallenge.java
+++ src/Basics/OperatorChallenge.java
@@ -2,14 +2,17 @@ package Basics;
 
 public class OperatorChallenge {
     public static void main(String[] args) {
-        double dValue = 20.00d;
-        double dValue2 = 80.00d;
-        double result = (dValue + dValue2) * 100;
-        double reminderRes = result % 40d;
-        boolean boolRes = reminderRes == 0;
-        System.out.println("boolRes = " + boolRes);
-        if (boolRes) {
-            System.out.println("got some reminder");
-        }
+//        double dValue = 20.00d;
+//        double dValue2 = 80.00d;
+//        double result = (dValue + dValue2) * 100;
+//        double reminderRes = result % 40d;
+//        boolean boolRes = reminderRes == 0;
+//        System.out.println("boolRes = " + boolRes);
+//        if (boolRes) {
+//            System.out.println("got some reminder");
+//        }
+
+        boolean readyForWork = true;
+        System.out.println(readyForWork);
     }
 }
