5ce1ee4ff29e0fe4e3fb9f1bfe4a69c8e493e54f
ibudnitski <igor.budnitski@gmail.com>
2023-12-15 23:24:01 +0200

    Equal Sum Checker challenge

--- src/Challenges/EqualSumChecker.java
+++ src/Challenges/EqualSumChecker.java
@@ -1,5 +1,11 @@
 package Challenges;
 
+/*
+* Write a method hasEqualSum with 3 parameters of type int.
+
+The method should return boolean and it needs to
+* return true if the sum of the first and second parameter is equal to the third parameter. Otherwise, return false.
+* */
 public class EqualSumChecker {
     public static void main(String[] args) {
 
