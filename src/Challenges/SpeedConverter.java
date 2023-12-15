4e65313b79a6d2b87197398e5d99e2e6daeeee9d
ibudnitski <igor.budnitski@gmail.com>
2023-12-15 13:15:27 +0200

    Speed Converter

---
+++ src/Challenges/SpeedConverter.java
@@ -0,0 +1,24 @@
+package Challenges;
+
+public class SpeedConverter {
+    public static void main(String[] args) {
+        printConversion(-25.42);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour / 1.609d);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+}
