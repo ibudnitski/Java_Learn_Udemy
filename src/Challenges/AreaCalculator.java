2ed9ae7b3f44b52d400c4c79c50eea6996f7731c
ibudnitski <igor.budnitski@gmail.com>
2023-12-16 15:02:34 +0200

    Area Calculator Challenge

---
+++ src/Challenges/AreaCalculator.java
@@ -0,0 +1,24 @@
+package Challenges;
+
+public class AreaCalculator {
+    public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0,4.0));
+    }
+
+    public static double area(double radius){
+        if (radius < 0 ){
+            return -1.0d;
+        }
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public static double area(double x, double y){
+        if (x < 0 || y < 0 ){
+            return -1.0d;
+        }
+        return x * y;
+    }
+}
