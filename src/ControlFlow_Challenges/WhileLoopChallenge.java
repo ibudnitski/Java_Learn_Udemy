a542d187704963bc1e2acaeb3a0f5aea7841e6cf
ibudnitski <igor.budnitski@gmail.com>
2023-12-18 19:26:59 +0200

    While loop challenge

--- src/ControlFlow_Challenges/WhileLoopChallenge.java
+++ src/ControlFlow_Challenges/WhileLoopChallenge.java
@@ -4,12 +4,20 @@ public class WhileLoopChallenge {
     public static void main(String[] args) {
         int counter = 4;
         int endNumber = 20;
+        int oddCounter = 0;
+        int evenCounter = 0;
         while (counter <= endNumber){
             if (isEven(counter)){
                 System.out.println("Even number: " + counter);
+                evenCounter++;
+            }else {
+                oddCounter++;
             }
             counter++;
         }
+
+        System.out.println("evenCounter = " + evenCounter);
+        System.out.println("oddCounter = " + oddCounter);
     }
 
     public static boolean isEven(int number) {
